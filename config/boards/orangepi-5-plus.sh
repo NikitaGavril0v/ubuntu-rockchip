5f61b2956c3bd2ded415e2d7088b61712bbce468
nagavrilov <nagavrilov@miem.hse.ru>
2025-02-19 20:53:45 +0300

    Add net-platform to board config

--- config/boards/orangepi-5-plus.sh
+++ config/boards/orangepi-5-plus.sh
@@ -40,7 +40,9 @@ function config_image_hook__orangepi-5-plus() {
         # Install OBS with gstreamer plugin
         cp "${overlay}/usr/lib/scripts/install-obs.sh" "${rootfs}/usr/lib/scripts/install-obs.sh"
         cp "${overlay}/usr/lib/scripts/libndi-get.sh" "${rootfs}/usr/lib/scripts/libndi-get.sh"
+        cp "${overlay}/usr/lib/scripts/net-platform.sh" "${rootfs}/usr/lib/scripts/net-platform.sh"
         chroot "${rootfs}" /usr/lib/scripts/install-obs.sh
+        chroot "${rootfs}" /usr/lib/scripts/net-platform.sh
     fi
 
     return 0
