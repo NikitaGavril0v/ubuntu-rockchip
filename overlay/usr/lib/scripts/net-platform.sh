fc513c326696e3d5676c248764759ffd18cc4b7f
Nikita Gavrilov <nikitos.zhivov@gmail.com>
2025-03-13 14:03:38 +0300

    Update net-platform.sh

--- overlay/usr/lib/scripts/net-platform.sh
+++ overlay/usr/lib/scripts/net-platform.sh
@@ -15,24 +15,23 @@ do
 done
 
 #Конфигурация портов
-rfkill unblock all
+#rfkill unblock all
 touch /etc/dhcpcd.conf
 echo denyinterfaces ${ports[1]} > /etc/dhcpcd.conf
 touch /etc/netplan/99_config.yaml
 echo "network:
   version: 2
-  renderer: networkd
+  renderer: NetworkManager
   ethernets:
     ${ports[1]}:
       addresses:
-        - 192.168.11.1/24
-      nameservers:
-          search: [google, googleAlt]
-          addresses: [8.8.8.8, 8.8.4.4]" > /etc/netplan/99_config.yaml
+        - 192.168.11.1/24" > /etc/netplan/99_config.yaml
 #Установка PiHole
-curl -sSL https://install.pi-hole.net | bash
+git clone --depth 1 https://github.com/pi-hole/pi-hole.git Pi-hole
+cd "Pi-hole/automated install/"
+bash basic-install.sh
 pihole setpassword
-pihole -a enabledhcp "192.168.11.50" "192.168.11.200" "192.168.11.1" "24" "local"
+sed -i 's/etc_dnsmasq_d = false/etc_dnsmasq_d = true/' /etc/pihole/pihole.toml
 touch /etc/dnsmasq.d/99-pts-lan.conf
 echo "# DHCP server setting
 dhcp-authoritative
@@ -43,19 +42,11 @@ dhcp-option=option:router,192.168.11.1
 # Add NTP server to DHCP
 dhcp-option=option:ntp-server,0.0.0.0" > /etc/dnsmasq.d/99-pts-lan.conf
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -A INPUT -i lo -j ACCEPT
 #Настроим правила для LAN.
-iptables -A INPUT -i ${ports[1]} -j ACCEPT
-iptables -A FORWARD -i ${ports[0]} -o ${ports[1]} -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-#Настроим правила для WLAN.
-#iptables -A FORWARD -i wlan0 -o ${ports[0]} -j ACCEPT
-#iptables -A FORWARD -i ${ports[0]} -o wlan0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-#Прогоним трафик между ethernet интерфейсами
-iptables -A FORWARD -i ${ports[0]} -o ${ports[1]} -j ACCEPT
 iptables -A FORWARD -i ${ports[1]} -o ${ports[0]} -j ACCEPT
+iptables -A FORWARD -i ${ports[0]} -o ${ports[1]} -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 #Настроим MASQUERADE.
 iptables -t nat -A POSTROUTING -o ${ports[0]} -j MASQUERADE
-
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 iptables-save > /etc/iptables.rules
 touch /etc/network/if-pre-up.d/iptables
