739f3d7d764008f0985081cc3f86a4ef7df82eb2
Nikita Gavrilov <nagavrilov@miem.hse.ru>
2025-03-29 15:58:21 +0300

    Complete install-companion.sh

--- overlay/usr/lib/scripts/install-companion.sh
+++ overlay/usr/lib/scripts/install-companion.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
+apt install -y npm yarn
+fnm install 18
+fnm use 18
+fnm default 18
+corepack enable
 curl https://raw.githubusercontent.com/bitfocus/companion-pi/main/install.sh | bash
+mkdir /opt/companion-module-dev
+cd /opt/companion-module-dev
+git clone https://git.miem.hse.ru/19102/bitfocus/companion/akai-fire-backlight.git
+git clone https://git.miem.hse.ru/19102/bitfocus/companion/companion-module-generic-http.git
+git clone https://git.miem.hse.ru/19102/bitfocus/companion/companion-module-generic-onvif-dev.git
+git clone https://git.miem.hse.ru/19102/bitfocus/companion/companion-module-obs-studio-dev.git
+git clone https://git.miem.hse.ru/19102/bitfocus/companion/companion-module-server-http.git
+git clone https://git.miem.hse.ru/19102/bitfocus/companion/companion-module-websocket-listener.git
+git clone https://git.miem.hse.ru/19102/bitfocus/companion/generic-midi.git
+git clone https://git.miem.hse.ru/19102/bitfocus/companion/generic-midi-winwet.git
+git clone https://git.miem.hse.ru/19102/bitfocus/companion/generic-pelco.git
+for dir in /opt/companion-module-dev/*/; do
+  (
+    cd "$dir"
+    npm install && npm run build
+  ) || (
+    cd "$dir"
+    yarn install && yarm build
+  )
+done
+systemctl enable companion
